fd8e789bf0995adc428668f6f537b1191ad66ef3
pasherlife <30339456+pasherlife@users.noreply.github.com>
2025-11-25 22:42:02 +0200

    Update build.gradle.kts

--- Netmirror/build.gradle.kts
+++ Netmirror/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
 cloudstream {
     
-    language = "hi"
+    language = "ua"
     // All of these properties are optional, you can safely remove them
 
     description = "Netflix, PrimeVideo, Disney+ Hotstar Contents in Multiple Languages"
